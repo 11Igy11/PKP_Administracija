08328188da7e1da845679d6c50b90889c0a92ea8
igi_t <iimpric@gmail.com>
2026-01-14 10:53:20 +0100

    Initial MVC project structure

---
+++ PKP_Administracija/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PKP_Administracija.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
