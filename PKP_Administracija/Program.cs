859a32426d2efe81817cd3b5fce6b51aee76848d
igi_t <iimpric@gmail.com>
2026-01-14 13:21:12 +0100

    Initial EF Core MySQL migration

--- PKP_Administracija/Program.cs
+++ PKP_Administracija/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PKP_Administracija.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    ));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
